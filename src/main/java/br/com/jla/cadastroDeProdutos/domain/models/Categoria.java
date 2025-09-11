24cd6dce1b550a3edd3909c5ac64c626aa70549e
Jhonatan <jhonatanlira@gmail.com>
2025-09-11 15:42:27 -0300

    criação das classes Produto e Categoria

---
+++ src/main/java/br/com/jla/cadastroDeProdutos/domain/models/Categoria.java
@@ -0,0 +1,42 @@
+package br.com.jla.cadastroDeProdutos.domain.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
