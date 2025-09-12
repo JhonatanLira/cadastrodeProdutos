1fcf3b74053b559ec6696b028e2efecf375cd099
Jhonatan <jhonatanlira@gmail.com>
2025-09-12 12:25:37 -0300

    criação dos services Categoria e Produto

--- src/main/java/br/com/jla/cadastroDeProdutos/domain/repositories/ProdutoRepository.java
+++ src/main/java/br/com/jla/cadastroDeProdutos/domain/repositories/ProdutoRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto,Long> {
 
     @Override
     List<Produto> findAll();
+
+    @Override
+    Optional<Produto> findById(Long id);
 
 }
