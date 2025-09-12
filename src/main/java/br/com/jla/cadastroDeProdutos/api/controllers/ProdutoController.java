28acd400422a25d6e526f01bb693963c1745a284
Jhonatan <jhonatanlira@gmail.com>
2025-09-12 16:04:03 -0300

    criação dos controller produto inacabado

--- src/main/java/br/com/jla/cadastroDeProdutos/api/controllers/ProdutoController.java
+++ src/main/java/br/com/jla/cadastroDeProdutos/api/controllers/ProdutoController.java
@@ -1,19 +1,37 @@
 package br.com.jla.cadastroDeProdutos.api.controllers;
 
+import br.com.jla.cadastroDeProdutos.domain.models.Produto;
+import br.com.jla.cadastroDeProdutos.domain.services.ProdutoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 @Tag(name = "produtos",description = "Descrição dos produtos")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoService produtoService;
+
+
     @GetMapping
     @Operation(summary = "Listar todos os produtos")
-    public void listarProdutos(){
-
+    public List<Produto> listarProdutos(){
+        return produtoService.listarTodosProdutos();
     }
+
+    @GetMapping("/id")
+    @Operation(summary = "busca produto por id")
+    public Produto buscaProduto(@RequestParam("id") Long id){
+        return ResponseEntity.ok(produtoService.buscaPorId(id)).getBody().get();
+    }
+
+
+
+
 }
